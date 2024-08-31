bfa60588f1d92a6b457d24160968efe10fa40334
anupamkhatiwada01 <anupamkhatiwada01@gmail.com>
2024-09-01 01:39:51 +0530

    using gemini model instead of gpt

--- 1_chat_models/1_chat_model_basic.py
+++ 1_chat_models/1_chat_model_basic.py
@@ -1,14 +1,28 @@
 # Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/
 # OpenAI Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/openai/
 
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+# from dotenv import load_dotenv
+# from langchain_openai import ChatOpenAI
+# from langchain_google_genai import ChatGoogleGenerativeAI
+
+from model import use_model
 
 # Load environment variables from .env
-load_dotenv()
+# load_dotenv()
 
 # Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+# model = ChatOpenAI(model="gpt-4o")  
+# model = ChatGoogleGenerativeAI(
+#     model="gemini-1.5-pro",
+#     temperature=0,
+#     max_tokens=None,
+#     timeout=None,
+#     max_retries=2,
+#     # other params...
+#     )
+
+model = use_model()
+
 
 # Invoke the model with a message
 result = model.invoke("What is 81 divided by 9?")
